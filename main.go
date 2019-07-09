e614956adff798ac61c2daf78ba9de918ae3fc7c
刘畅 <liuchang@liuchangdeMacBook-Pro.local>
2019-07-09 15:55:43 -0400

    first initializtion

--- main.go
+++ main.go
@@ -1,73 +1,160 @@
 package main
+
 import (
-	"fmt"
 	"encoding/json"
-	"net/http"
+	"fmt"
+	"github.com/pborman/uuid"
+	elastic "gopkg.in/olivere/elastic.v3"
 	"log"
+	"net/http"
+	"reflect"
 	"strconv"
 )
 
-type Location struct{
+type Location struct {
 	Lat float64 `json: "lat"`
 	Lon float64 `json: "lon"`
 }
 
-type Post struct{
-	User string `json: "user"`//exported name
-	Message string `json: "message"`
+type Post struct {
+	User     string   `json: "user"` //exported name
+	Message  string   `json: "message"`
 	Location Location `json: "location"`
 }
 
-const(
-	DISTANCE =  "200km"
+const (
+	INDEX    = "around"
+	TYPE     = "post"
+	DISTANCE = "200km"
+	ES_URL   = "http://104.197.206.142:9200"
 )
 
-func main(){
+func main() {
+	// Create a client
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	// Use the IndexExists service to check if a specified index exists.
+	exists, err := client.IndexExists(INDEX).Do()
+	if err != nil {
+		panic(err)
+	}
+	if !exists {
+		// Create a new index.
+		mapping := `{
+			"mappings":{
+				"post":{
+					"properties":{
+						"location":{
+							"type":"geo_point"
+						}
+					}
+				}
+			}
+		}`
+		_, err := client.CreateIndex(INDEX).Body(mapping).Do()
+		if err != nil {
+			// Handle error
+			panic(err)
+		}
+	}
 
 	fmt.Println("started-service")
 	http.HandleFunc("/post", handlerPost)
 	http.HandleFunc("/search", handlerSearch)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-	
+
 }
 
-func handlerPost(w http.ResponseWriter, r *http.Request){
+func handlerPost(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Receeived one post request.")
 	decoder := json.NewDecoder(r.Body)
 	var p Post
-	if err := decoder.Decode(&p); err != nil{
+	if err := decoder.Decode(&p); err != nil {
 		panic(err)
 	}
 	fmt.Fprintf(w, "Post received: %s\n", p.Message)
 
+	id := uuid.New()
+	//Save to ES
+	saveToES(&p, id)
+
 }
 
-func handlerSearch(w http.ResponseWriter, r *http.Request){
+// Save a post to ElasticSearch
+func saveToES(p *Post, id string) {
+	// Create a client
+	es_client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	// Save it to index
+	_, err = es_client.Index().
+		Index(INDEX).
+		Type(TYPE).
+		Id(id).
+		BodyJson(p).
+		Refresh(true).
+		Do()
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	fmt.Printf("Post is saved to Index: %s\n", p.Message)
+}
+
+func handlerSearch(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("recieved one request for search.")
 
 	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
 	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
 
 	ran := DISTANCE
-	if val := r.URL.Query().Get("range"); val != ""{
+	if val := r.URL.Query().Get("range"); val != "" {
 		ran = val + "km"
 	}
-	
-	fmt.Println("range is ", ran)
-	//return a fake post
-	p := &Post{
-		User:"1111",
-		Message: "一生必去的100个地方",
-		Location: Location{
-			Lat: lat,
-			Lon: lon,
-		},
+
+	fmt.Println("Elastic Search ", ran)
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+	}
+	q := elastic.NewGeoDistanceQuery("location")
+	q = q.Distance(ran).Lat(lat).Lon(lon)
+
+	searchResult, err := client.Search().
+		Index(INDEX).
+		Query(q).
+		Pretty(true).
+		Do()
+	if err != nil {
+		panic(err)
 	}
 
-	js, err := json.Marshal(p)
+	fmt.Printf("Query took %d milliseconds\n", searchResult.TookInMillis)
+	fmt.Printf("Found a total of %d post\n", searchResult.TotalHits())
 
+	var typ Post
+	var ps []Post
+	for _, item := range searchResult.Each(reflect.TypeOf(typ)) {
+		p := item.(Post)
+		fmt.Printf("Post by %s: %s at lat %v and lon %v\n", p.User, p.Message, p.Location.Lat, p.Location.Lon)
+
+		ps = append(ps, p)
+	}
+	js, err := json.Marshal(ps)
+	if err != nil {
+		panic(err)
+		return
 	}
 
-	w.Header().Set("Conten-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(js)
 }
